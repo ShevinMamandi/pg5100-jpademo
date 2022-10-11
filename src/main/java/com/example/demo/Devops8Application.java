295799334e65982a13044e569d2fb1539bfbd654
Shevin Mamandi <shevin1987@gmail.com>
2022-10-11 11:17:44 +0200

    new projekt

---
+++ src/main/java/com/example/demo/Devops8Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Devops8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Devops8Application.class, args);
+	}
+
+}
